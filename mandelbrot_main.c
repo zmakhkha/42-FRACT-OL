926bf9665ff1b5fe6829f489dda4defb2a7662b9
zmksiii <jcaladic@gmail.com>
2023-01-29 18:49:24 +0100

    -> Zoom working <> Still not following the nouse position

--- mandelbrot_main.c
+++ mandelbrot_main.c
@@ -6,7 +6,7 @@
 /*   By: zmakhkha <zmakhkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 19:17:32 by zmakhkha          #+#    #+#             */
-/*   Updated: 2023/01/27 22:25:00 by zmakhkha         ###   ########.fr       */
+/*   Updated: 2023/01/29 18:47:25 by zmakhkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	ft_initiate_mandelbrot(t_vars **d, int height, int width)
 	t_vars	*data;
 
 	data = *d;
-	//data -> x = -1;
-	//data -> y = -1;
+	data -> x = -1;
+	data -> y = -1;
 	data -> n = -1;
 	data -> m_iter = 50;
 	data -> height = height;
@@ -56,16 +56,21 @@ void	ft_put_pixel_mandelbrot(t_vars **d)
 			break ;
 	}
 	if (data -> n < data -> m_iter)
-		data->color = 0xFFFFFF;
+		data->color = 0xFC7300 * data -> n % 150;
 	else
-		data->color = 0x645CBB;
+		data->color = 0xFFFFFF;
 	my_mlx_pixel_put(&(data -> img), data -> x, data -> y, data ->color);
 }
 
-int_least32_t	ft_mandelbrot(t_vars **d)
+int	ft_mandelbrot(t_vars **d)
 {
-	t_vars *data = *d; 
-	ft_initiate_mandelbrot(&data, 500, 500);
+	t_vars *data = *d;
+
+	data -> x = -1;
+	data -> y = -1;
+	data ->img.img = mlx_new_image(data -> mlx, data -> width, data -> height);
+	data ->img.addr = mlx_get_data_addr(data -> img.img, &(data ->img.bits_per_pixel), \
+	&(data -> img.line_length), &(data ->img.endian));
 	while (++(data -> x) < data ->width)
 	{
 		data -> y = -1;
@@ -78,44 +83,40 @@ int_least32_t	ft_mandelbrot(t_vars **d)
 int	mouse_hook(int keycode, int x, int y, t_vars *a)
 {
 
+	(void)x;
+	(void)y;
 	t_vars *vars = (t_vars *)a;
 	if (keycode == 5)
 	{
-		vars ->scale *= .7;
-		vars -> x = x;
-		vars -> y = y;
-		ft_mandelbrot(&vars);
+		vars ->scale /= .8;
+		vars -> x = vars -> x + (vars -> width - vars -> x) * vars -> scale;
 	}
 	if (keycode == 4)
 	{
-		vars ->scale /= .7;
-		vars -> x = x;
-		vars -> y = y;
-		ft_mandelbrot(&vars);
+		vars ->scale *= .8;
+		vars -> y = vars -> y + (vars -> width - vars -> y) * vars -> scale;
 	}
+	mlx_destroy_image(vars -> mlx, vars -> img.img);
+	ft_mandelbrot(&vars);
 	
 	return (0);
 }
+
+
 int	main(void)
 {
 	t_vars *data;
 
 	data = (t_vars *)malloc(sizeof(t_vars));
+	ft_initiate_mandelbrot(&data, 750, 750);
 	data ->mlx = mlx_init();
-	data ->win = mlx_new_window(data -> mlx, 500, 500, "Mandelbrot Set");
-	data ->img.img = mlx_new_image(data -> mlx, 500, 500);
-	data ->img.addr = mlx_get_data_addr(data -> img.img, &(data ->img.bits_per_pixel), \
-	&(data -> img.line_length), &(data ->img.endian));
+	data ->win = mlx_new_window(data -> mlx, data -> width, data -> height, "Mandelbrot Set");
+
 	data -> scale = 4;
-	data -> x = -1;
-	data -> y = -1;
 	ft_mandelbrot(&data);
-	//mlx_mouse_hook(data ->win, mouse_hook, data);
-	//mlx_loop_hook(data ->mlx, ft_mandelbrot, data);
 	mlx_hook(data -> win, 2, 1L << 5, key_hook, data);
 	mlx_hook(data -> win, 17, 1L << 0, destroy, data);
 	mlx_hook(data -> win, 4, 1L<<7, mouse_hook, data);
-	//mlx_mouse_hook(data -> win, mouse_hook, &data);
 	mlx_loop(data -> mlx);
 	//free (data);
 }
